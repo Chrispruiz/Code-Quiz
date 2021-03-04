54751fbd7c50db068279c0583ddf0c65a1af949f
Chris Ruiz <chrispruiz@att.net>
2021-03-04 01:04:23 -0600

    fixed timer, added questions, fixed back button

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,59 +1,57 @@
-// creating an array and passing the number, questions, optionms, and answers
 let questions = [
     {
-        numb: 1,
-        question: "What is 1+1?",
-        answer: "the",
-        options: [
-            "the",
-            "1",
-            "3",
-            "4"
-        ]
-    },
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
     {
-        numb: 2,
-        question: "2Question",
-        answer: "answer2",
-        options: [
-            "answer1",
-            "answer2",
-            "answer3",
-            "answer4"
-        ]
-    },
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
     {
-        numb: 3,
-        question: "3Question",
-        answer: "answer3",
-        options: [
-            "answer1",
-            "answer2",
-            "answer3",
-            "answer4"
-        ]
-    },
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
     {
-        numb: 4,
-        question: "4Question",
-        answer: "answer4",
-        options: [
-            "answer1",
-            "answer2",
-            "answer3",
-            "answer4"
-        ]
-    },
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
     {
-        numb: 5,
-        question: "5Question",
-        answer: "answer5",
-        options: [
-            "answer1",
-            "answer2",
-            "answer3",
-            "answer4"
-        ]
-    },
-    
-]
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const start_btn = document.querySelector(".start_btn button");
 const quiz_box = document.querySelector(".quiz_box");
 const timeCount = quiz_box.querySelector(".timer .timer_sec");
 
+
+
 // if start button clicked 
 start_btn.onclick = ()=> {
     quiz_box.classList.add("activeQuiz"); // hide start button
@@ -27,40 +29,56 @@ option.onclick = ()=> {
         console.log("Questions completed")
         showResultBox();
     }
-} 
+}  
+
 // getting questions and options from array
 function showQuestions(index) {
     const que_text = document.querySelector(".que_text");
     const option_list = document.querySelector(".option_list");
     let que_tag = '<span>'+ questions[index].numb + '. ' + questions[index].question +'</span>';
-    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
-                    + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
-                    + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
-                    + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+                    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+                    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+                    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
 
-    const option = option_list.querySelectorAll(".option");
-    for (let i = 0; i < option.length; i++) {
-        option[i].setAttribute("onlick", "optionSelected(this)");
+    const option = option_list.querySelectorAll('.option');
+    for (i = 0; i < option.length; i++) {
+        option[i].setAttribute('onlick', 'optionSelected(this)');
     } 
 }
 
-function optionSelected(answer){
-    let userAns = answer.textContent;
+function optionSelected(answer) {
+    let userAns = options.textContent;
     console.log(userAns);
-    
+}
+
+// if quitQuiz button clicked
+restart_quiz.onclick = ()=>{
+    window.location.reload(); //reload the current window
 }
 
 function showResultBox() {
     quiz_box.classList.remove("activeQuiz"); // hide quiz box
     result_box.classList.add("activeResult"); // show result box
+    
+    const score_text = document.querySelector(".score_text");
+    let score_tag = 'Your score is' + counter;
+    score_text.innerHTML = score_tag;
+    
 }
-
+// timer 
 function startTimer(time){
     counter = setInterval(timer, 1000);
     function timer() {
         timeCount.textContent = time;
         time--;
+        if (time < 0) {
+            clearInterval(counter)
+            quiz_box.classList.remove("activeQuiz");
+            result_box.classList.add("activeResult");
+        }
     }
 }
+
